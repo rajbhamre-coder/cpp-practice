cab046b7acdc212f31a9392b3c48b2de89bd83d5
rajbhamre-coder <rajbhamre27@gmail.com>
2026-06-23 17:12:48 +0530

    so this include all operators of cpp which performs different operation on variables

---
+++ BASICS/variables/operators.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a, b;
+    cout<<"please enter a:";
+    cin>>a;
+    cout<<"please enter b:";
+    cin>>b;
+    cout<<"the sum of a and b is:"<<a+b<<endl;
+    cout<<"the difference of the a and b is:"<<a-b<<endl;
+    cout<<"the product of the numbr a and b is :"<<a*b<<endl;
+    cout<<"the remainder of the number a and b is:"<<a%b<<endl;
+    return 0;
+}
