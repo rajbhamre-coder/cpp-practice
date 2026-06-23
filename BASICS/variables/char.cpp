e760d7e513bd05444abf7976ac73f8c8e8699e3e
rajbhamre-coder <rajbhamre27@gmail.com>
2026-06-23 16:56:04 +0530

    some basic program on variables

---
+++ BASICS/variables/char.cpp
@@ -0,0 +1,11 @@
+// char datatype stored the alphabets in its ascii values 
+#include <iostream>
+using namespace std;
+
+int main() {
+    char grade = 'A';
+
+    cout << (int)grade << endl;
+
+    return 0;
+}
---
+++ BASICS/variables/size_fx.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int age=25;
+    cout<<sizeof(age)<<endl;
+    return 0;
+}
