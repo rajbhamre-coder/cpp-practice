1e3a81cc92975e81b894256b3d8de16e61bbf8b8
rajbhamre-coder <rajbhamre27@gmail.com>
2026-06-24 16:30:31 +0530

    loops commit

---
+++ BASICS/loops/dowhileloop.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main(){
+    do{
+        cout<<"hello rajju boiii"<<endl;
+    } while(3<5);
+}
--- BASICS/loops/forloop.cpp
+++ BASICS/loops/forloop.cpp
@@ -6,4 +6,15 @@ int main(){
     for(int i=1;i<=n;i++){
         cout<<i<<endl;
     }
+
+
+// another program to print sum of n numbers.
+int n;
+cout<<"enter the number till which you want the sum"<<endl;
+cin>>n;
+int count=0;
+for(int i=1;i<=n;i++){
+    count++;
+}
+cout<<"the sum of numbers from 1 to "<<n<<" is: "<<count<<endl;
 }
