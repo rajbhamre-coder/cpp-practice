df4688af9cf40259c861860e03ed7a7b9aabe0b7
rajbhamre-coder <rajbhamre27@gmail.com>
2026-06-24 11:10:02 +0530

    this is a ternary statement operator example in cpp

---
+++ BASICS/conditional/ternary_statement.cpp
@@ -0,0 +1,15 @@
+// here we have to give an condition then in right side if its true it will give tht cout nd if nnot then it will give false statement print which is again right side of the true one.
+#include <iostream>
+using namespace std;
+int main(){
+    int n=45;
+    cout<<(n>=0?"n is positive":"n is negative");
+
+
+// another program for ternary operator
+    int m;
+    cout<<"enter your age miss/mis";
+    cin>>m;
+    cout<<(m>=18?"youre eligible to vote dear":"youre not elligible dear sorry go bac to your mumma ");
+    return 0;
+}
