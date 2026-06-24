f0449a41991a3a1f7a47e083f61e8ff0b7fd535c
rajbhamre-coder <rajbhamre27@gmail.com>
2026-06-24 11:29:32 +0530

    this is a for lioop operator

---
+++ BASICS/loops/forloop.cpp
@@ -0,0 +1,9 @@
+// now this is a for loop here wilk have to put initilisation then condition them updation all in one ith seperation";" so here is few questionss
+#include <iostream>
+using namespace std;
+int main(){
+    int n=10;
+    for(int i=1;i<=n;i++){
+        cout<<i<<endl;
+    }
+}
---
+++ BASICS/loops/whileloop.cpp
@@ -0,0 +1,12 @@
+//this loop operator will continues till its condition is true and it will stop when the condition is false;
+#include <iostream>
+using namespace std;
+int main(){
+    int count=1;
+    while(count<=10){
+        cout<<count<< " ";
+        count++;
+    
+    }
+    return 0;
+}
