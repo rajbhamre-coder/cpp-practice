0f362feb95d540a5fc2265798cc3a1fbae50a444
rajbhamre-coder <rajbhamre27@gmail.com>
2026-07-07 14:09:50 +0530

    1 to n numbers print function

---
+++ FUNCTION/1ton.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int oneton(int n){
+    for (int i=1;i<=n;i++){
+        cout<<i<<endl;
+    }return 0;
+}
+
+
+
+int main (){
+    oneton(58);
+    oneton(33);
+    oneton(10);
+    return 0;
+}
