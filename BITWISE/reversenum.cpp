1f0e49be9ea2afbcf52752ac0d3b712800693d78
rajbhamre-coder <rajbhamre27@gmail.com>
2026-07-09 16:47:19 +0530

    reverse

---
+++ BITWISE/reversenum.cpp
@@ -0,0 +1,9 @@
+// here we can reverse the given number also use here long long bitwise operator as we have to enter large numbers which take to much spacessss
+#include <iostream>
+using namespace std;
+int main(){
+    long long n;
+    cout<<"enter the number u want to reverse"<<endl;
+    cin>>n;
+    for(int i=0;)
+}
